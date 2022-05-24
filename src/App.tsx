d4d0b4d11f2405e9c67e90843cf2b3a7da1188c5
tobiasny <tobnyg@hotmail.no>
2022-05-24 19:40:39 +0200

    add authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,110 @@
-import React from 'react';
+import { AccountInfo, InteractionRequiredAuthError, InteractionStatus, InteractionType, IPublicClientApplication } from '@azure/msal-browser';
+import { MsalAuthenticationTemplate, useMsal } from '@azure/msal-react';
+import React, { useEffect, useState } from 'react';
 import logo from './ACME.png';
 import './App.css';
 
+const authRequest = {
+  scopes: ["User.Read"],
+};
+
+type GraphData = {
+  businessPhones: string[];
+  displayName: string;
+  givenName: string;
+  jobTitle: string;
+  mail: string;
+  mobilePhone: string;
+  officeLocation: string;
+  preferredLanguage: string;
+  surname: string;
+  userPrincipalName: string;
+  id: string;
+}
+
+const me_url = 'https://graph.microsoft.com/v1.0/me/';
+const me_photo_url = 'https://graph.microsoft.com/v1.0/me/photo/$value';
+
+async function callMsGraph(url: string, msalInstance: IPublicClientApplication) {
+  const account = msalInstance.getActiveAccount();
+  if (!account) {
+      throw Error("No active account! Verify a user has been signed in and setActiveAccount has been called.");
+  }
+
+  const response = await msalInstance.acquireTokenSilent({
+      ...authRequest,
+      account: account
+  });
+
+  const headers = new Headers();
+  const bearer = `Bearer ${response.accessToken}`;
+
+  headers.append("Authorization", bearer);
+
+  const options = {
+      method: "GET",
+      headers: headers
+  };
+
+  return await fetch(url, options);
+}
+
+async function getMe(msalInstance: IPublicClientApplication) {
+  const response = await callMsGraph(me_url, msalInstance);
+  return await response.json();
+}
+
+async function getPhoto(msalInstance: IPublicClientApplication) {
+  const response = await callMsGraph(me_photo_url, msalInstance);
+
+  if (!response.ok) return null;
+
+  const blob = await response.blob();
+  return URL.createObjectURL(blob);
+}
+
 function App() {
+  const { instance, inProgress } = useMsal();
+  const [ graphData, setGraphData ] = useState<GraphData | null>(null);
+  const [ photo, setPhoto ] = useState<string | null>(null);
+
+  useEffect(() => {
+    const acquireToken = () => {
+      instance.acquireTokenRedirect({
+        ...authRequest,
+        account: instance.getActiveAccount() as AccountInfo
+      });
+    }
+    if (!graphData && inProgress=== InteractionStatus.None) {
+      getMe(instance)
+        .then(setGraphData)
+        .catch((e) => { if (e instanceof InteractionRequiredAuthError) acquireToken(); });
+      getPhoto(instance)
+        .then(setPhoto)
+        .catch((e) => { if (e instanceof InteractionRequiredAuthError) acquireToken(); });
+    }
+  }, [instance, graphData, photo, inProgress])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my awesome app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MsalAuthenticationTemplate
+      interactionType={InteractionType.Redirect}
+      authenticationRequest={authRequest}
+    >
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Logged in as {graphData?.displayName}
+          </p>
+          <img src={photo || logo} className="App-logo" alt="currently logged in user" />
+          <button
+            className="App-link"
+            onClick={() => instance.logoutRedirect()}
+          >
+            Log out
+          </button>
+        </header>
+      </div>
+    </MsalAuthenticationTemplate>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthenticationResult, Configuration, EventMessage, EventType, PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+
+const redirectUri = () => {
+  const location = document.location;
+  if (location.hostname === 'localhost') {
+    console.log(`${location.protocol}//${location.hostname}:${location.port}${location.pathname}`)
+    return `${location.protocol}//${location.hostname}:${location.port}${location.pathname}`
+  }
+  
+  return `${location.protocol}//${location.hostname}${location.pathname}`
+}
+const configuration: Configuration = {
+  auth: {
+    clientId: '69d4c8a4-52c4-478f-9361-b7860e8c7018',
+    authority: 'https://login.microsoftonline.com/7d13004c-123c-4064-9291-3ebefd2822df',
+    redirectUri: redirectUri()
+  },
+}
+
+const pca = new PublicClientApplication(configuration);
+
+const accounts = pca.getAllAccounts();
+if (accounts.length > 0) {
+    pca.setActiveAccount(accounts[0]);
+}
+pca.addEventCallback((event: EventMessage) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+      const payload = event.payload as AuthenticationResult;
+      const account = payload.account;
+      pca.setActiveAccount(account);
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MsalProvider instance={pca}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MsalProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
